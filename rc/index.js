6db3f59869e7f0feac2a54258959ac4c24752b69
Oscar Guzman <oscar.guzman@hoytrabajas.com>
2020-06-29 14:45:30 -0500

    Update from master

---
+++ rc/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const io = require("socket.io");
+const http = require("http");
+const app = express();
+
+const cors = require("cors");
+app.use(cors());
+
+require("./config/database");
+
+const server = http.createServer(app);
+
+const baseIO = io(server);
+baseIO.origins("*:*");
+
+// esto es otro cambio
+// Afh's change
+// Andres es gei
+
+const privateIO = baseIO.of("/private");
+
+// esto es un cambio para el repo
+
+const athorizacion = require("./middlewares/athorizacionSocket");
+
+privateIO.use(athorizacion);
+
+module.exports.io = baseIO;
+module.exports.privateIO = privateIO;
+
+require("./sockets");
+
+app.get("/", (r, s) => s.json({ winiie: "ok" }));
+
+server.listen(process.env.PORT, () => console.log(`Socket Server`));
